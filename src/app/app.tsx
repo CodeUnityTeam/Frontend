9d3aca48c147859a4869a376bb3b1698015b9175
zang3tsu88 <maximenya.a@gmail.com>
2026-05-11 11:56:06 +0300

    refactor: remove demo code

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,8 @@
-import { Icon } from "@iconify/react";
 import { Outlet } from "react-router";
 
 export function App() {
   return (
     <div className="app-layout">
-      {
-        // TODO: Демонстрация использования иконок
-        // дока:
-        // TLDR: https://www.npmjs.com/package/@iconify/react
-        // Official: https://iconify.design/docs/iconify-icon/
-        // так же на сайте можно ознакомиться с обильной библиотекой паков иконок.
-      }
-      <Icon
-        icon="ph:eye"
-        className="text-7xl text-purple-600 mx-auto h-dvh hover:text-purple-400 cursor-pointer transition"
-      />
       {/* тут размещается sidebar */}
       <Outlet />
     </div>
