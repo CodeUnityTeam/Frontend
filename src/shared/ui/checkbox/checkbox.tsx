0b6b045206229e6e254b9c145f107a58a5c28779
zang3tsu88 <maximenya.a@gmail.com>
2026-05-17 00:16:46 +0300

    feat: implement checkbox

---
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+
+import { cn } from "@/shared/lib/utils";
+import { Icon } from "@iconify/react";
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "grid size-7 shrink-0 place-content-center rounded-xs border border-border ring-offset-background transition-colors hover:border-primary hover:bg-secondary-hover focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-disabled-foreground disabled:bg-disabled data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground disabled:data-[state=checked]:border-disabled-foreground disabled:data-[state=checked]:bg-disabled disabled:data-[state=checked]:text-disabled-foreground",
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("grid place-content-center text-current")}
+    >
+      <Icon icon="lucide:check" className="size-6" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
