5ac17abbec52ee69af114988a3a725d7a79cd022
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2026-05-27 18:49:49 +0300

    fix(router): fix lazy loading for about page

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router";
 import { App } from "@/app/app";
-import AboutPage from "@/pages/about/about-page";
 import { HomePage } from "@/pages/home/ui/home-page";
 
 export const router = createBrowserRouter([
@@ -8,7 +7,6 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />, // layout со всеми providers и sidebar
     children: [
-      { path: "about", element: <AboutPage /> },
       {
         index: true,
         element: <HomePage />,
@@ -28,7 +26,16 @@ export const router = createBrowserRouter([
        */
       { path: "projects", lazy: () => import("@/pages/projects/projects-page") },
       { path: "qa", lazy: () => import("@/pages/qa/qa-page") },
-      { path: "about", lazy: () => import("@/pages/about/about-page") },
+      {
+        path: "about",
+        lazy: async () => {
+          const module = await import("@/pages/about/about-page");
+
+          return {
+            Component: module.default,
+          };
+        },
+      },
       { path: "help", lazy: () => import("@/pages/help/help-page") },
       { path: "documents", lazy: () => import("@/pages/documents/documents-page") },
     ],
