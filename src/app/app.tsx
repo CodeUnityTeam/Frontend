11becb22bc46fd6b3ed224aac7732d08c2503a8b
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2026-05-28 19:09:22 +0500

    feat: установить все зависимости, сверстать блок чат бот.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/widgets/header";
+import { ChatBotCard } from "@/widgets/header/ui/project-card";
 import { Outlet } from "react-router";
 
 export function App() {
@@ -7,6 +8,19 @@ export function App() {
       {/* тут размещается sidebar */}
       <Header />
       <Outlet />
+      <ChatBotCard
+        title="Чат-бот «CodeBuddy»"
+        description="Разрабатываем Телеграм-бота, который отвечает на типовые вопросы по отладке, синтаксису и заданиям из курсов."
+        tags={[
+          "Python",
+          "Flask/FastAPI",
+          "SQLite",
+          "Telegram Bot API",
+          "Figma",
+        ]}
+        date="15.04"
+        location="Нижний Новгород"
+      />
     </div>
   );
 }
