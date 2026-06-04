4cdd4f4bb69e259085309cde2616cd89a10494fa
zang3tsu88 <maximenya.a@gmail.com>
2026-06-04 19:41:05 +0300

    fix: remove sonner dark/light  theme pack

--- src/shared/ui/sonner/sonner.tsx
+++ src/shared/ui/sonner/sonner.tsx
@@ -1,15 +1,11 @@
 import { Icon } from "@iconify/react";
-import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
         success: <Icon icon="lucide:circle-check" className="h-4 w-4" />,
