fbf6fd4728bb8ea8501e9f61d638664ddbb9bc1d
zang3tsu88 <maximenya.a@gmail.com>
2026-06-08 22:10:32 +0300

    refactor: move toaster to providers

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,16 @@
 import { Header } from "@/widgets/header";
 import { Outlet } from "react-router";
 import { Footer } from "@/widgets/footer";
-import { Toaster } from "@/shared/ui/sonner";
+import { Providers } from "./providers";
 
 export function App() {
   return (
-    <div className="app-layout">
-      <Header />
-      <Outlet />
-      <Footer />
-      <Toaster closeButton />
-    </div>
+    <Providers>
+      <div className="app-layout">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </Providers>
   );
 }
