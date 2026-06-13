385329c3abcb7f4613851f14f2a861bde5f2edcf
soulgado <nleonovq@gmail.com>
2026-06-13 16:21:13 +0200

    fix/fixed mobile adaptation for SetSkillsForm, formatting

--- src/shared/ui/text-input/text-input.tsx
+++ src/shared/ui/text-input/text-input.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { FieldDescription, FieldLabel, FieldError, Field, FieldContent } from "../field";
+import {
+  FieldDescription,
+  FieldLabel,
+  FieldError,
+  Field,
+  FieldContent,
+} from "../field";
 import { InputGroup, InputGroupInput } from "../input/input-group";
 import { cn } from "@/shared/lib/utils";
 
@@ -13,41 +19,46 @@ interface TextInputProps extends React.ComponentProps<"input"> {
 }
 
 const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
-  ({
-    label,
-    description,
-    error,
-    placeholder,
-    className,
-    inputClassName,
-    ...props
-}, ref) => {
-  const inputId = React.useId();
+  (
+    {
+      label,
+      description,
+      error,
+      placeholder,
+      className,
+      inputClassName,
+      ...props
+    },
+    ref,
+  ) => {
+    const inputId = React.useId();
 
-  return (
-    <Field orientation="vertical" className={cn("gap-5", className)}>
-      <div className="flex flex-col gap-2">
-        {label && <FieldLabel htmlFor={inputId}>{label}</FieldLabel>}
-        {description && <FieldDescription id={inputId}>{description}</FieldDescription>}
-      </div>
-      <FieldContent>
-        <InputGroup>
-          <InputGroupInput
-            ref={ref}
-            id={inputId}
-            placeholder={placeholder}
-            className={cn(
-              "h-15",
-              inputClassName
-            )}
-            {...props}
-          />
-        </InputGroup>
-        {error && <FieldError>{error}</FieldError>}
-      </FieldContent>
-    </Field>
-  )
-});
+    return (
+      <Field orientation="vertical" className={cn("gap-1 md:gap-5", className)}>
+        <div className="flex flex-col gap-2">
+          {label && <FieldLabel htmlFor={inputId}>{label}</FieldLabel>}
+          {description && (
+            <FieldDescription id={inputId} className="hidden md:block">
+              {description}
+            </FieldDescription>
+          )}
+        </div>
+        <FieldContent>
+          <InputGroup>
+            <InputGroupInput
+              ref={ref}
+              id={inputId}
+              placeholder={placeholder}
+              className={cn("h-15", inputClassName)}
+              {...props}
+            />
+          </InputGroup>
+          {error && <FieldError>{error}</FieldError>}
+        </FieldContent>
+      </Field>
+    );
+  },
+);
 
 TextInput.displayName = "TextInput";
 
--- src/widgets/set-skills-modal/ui/set-skills-modal.tsx
+++ src/widgets/set-skills-modal/ui/set-skills-modal.tsx
@@ -1,54 +1,69 @@
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Button } from "@/shared/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogOverlay, DialogTitle } from "@/shared/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogTitle,
+} from "@/shared/ui/dialog";
 import { TextInput } from "@/shared/ui/text-input";
 import type { SkillsFormData } from "@/widgets/account/model/types";
 
 type setSkillsModalProps = {
   open: boolean;
-  onOpenChange: (open: boolean) => void,
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void,
-  onChange: (field: keyof SkillsFormData) => (e: React.ChangeEvent<HTMLInputElement>) => void,
-  formData: SkillsFormData
-}
+  onOpenChange: (open: boolean) => void;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  onChange: (
+    field: keyof SkillsFormData,
+  ) => (e: React.ChangeEvent<HTMLInputElement>) => void;
+  formData: SkillsFormData;
+};
 
 export function SetSkillsModal({
   open,
   onOpenChange,
   onSubmit,
   onChange,
-  formData
+  formData,
 }: setSkillsModalProps) {
-  console.log(formData);
   return (
-    <Dialog open={open} onOpenChange={onOpenChange} >
+    <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogOverlay />
-      <DialogContent className="max-w-full md:max-w-[714px]">
+      <DialogContent className="mx-auto p-4 pbs-12 md:max-w-[714px] md:p-8 md:pbs-16">
         <VisuallyHidden>
           <DialogTitle>Форма навыков</DialogTitle>
-          <DialogDescription>Форма заполнения навыков, личных качеств и информации о себе</DialogDescription>
+          <DialogDescription>
+            Форма заполнения навыков, личных качеств и информации о себе
+          </DialogDescription>
         </VisuallyHidden>
-        <form onSubmit={onSubmit} className="flex flex-col gap-6 p-8">
-          <TextInput 
+        <form onSubmit={onSubmit} className="flex flex-col gap-4 md:gap-6">
+          <TextInput
             label="Навыки и инструменты"
             placeholder="Начните вводить здесь"
             description="Выберите программы, которыми вы владеете"
             value={formData.skills.join(", ")}
             onChange={onChange("skills")}
-            />
-          <TextInput 
+          />
+          <TextInput
             label="Личные качества"
             placeholder="Ваши преимущества"
             value={formData.qualities.join(", ")}
             onChange={onChange("qualities")}
-            />
-          <TextInput 
+          />
+          <TextInput
             label="О себе"
             placeholder="Расскажите о себе"
             value={formData.about}
             onChange={onChange("about")}
-            />
-          <Button type="submit" variant="default" className="mbs-6 md:max-w-33 self-center">Сохранить</Button>
+          />
+          <Button
+            type="submit"
+            variant="default"
+            className="w-full self-center md:mbs-6 md:max-w-33"
+          >
+            Сохранить
+          </Button>
         </form>
       </DialogContent>
     </Dialog>
