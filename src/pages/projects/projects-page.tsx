f9997cb2ec00e2c34d3928ee9e7f0ca9b674a4d7
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2026-06-20 21:10:17 +0300

    feat: add tabs widget

--- src/pages/projects/projects-page.tsx
+++ src/pages/projects/projects-page.tsx
@@ -10,8 +10,13 @@ import {
   FiltersMobile,
 } from "@/widgets/filters";
 import { Search } from "@/widgets/search";
+import { FilterTabs } from "@/widgets/filter-tabs";
+import { projectTabs } from "@/widgets/filter-tabs/model/tabs-data";
+import { useState } from "react";
 
 function ProjectsPage() {
+  const [tab, setTab] = useState("catalog");
+
   if (!isAuth) {
     return <Navigate to={ROUTES.REGISTER} replace />;
   }
@@ -24,10 +29,10 @@ function ProjectsPage() {
         <PageContainer className="py-8">
           <FiltersMobile className="mb-6 md:hidden" />
           <FiltersBar className="mb-6 hidden md:flex" />
-
+          
           <div className="md:flex md:items-start md:gap-8">
             <FiltersSidebar className="hidden md:block" />
-
+            <FilterTabs items={projectTabs} value={tab} onValueChange={setTab} />
             <div className="flex-1"></div>
           </div>
         </PageContainer>
--- src/widgets/tabs/index.ts
+++
@@ -1,1 +0,0 @@
-export {TabsLine} from './ui/tabs'
