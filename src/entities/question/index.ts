fcae618da1ffa3a88437adb411f3c37bfe62ee0b
zang3tsu88 <maximenya.a@gmail.com>
2026-06-21 12:10:43 +0300

    feat: integrate bd skills catalog into qa forms

--- src/entities/question/index.ts
+++ src/entities/question/index.ts
@@ -1,3 +1,2 @@
-export { questionTags, getTagLabels } from "./model/question-tags";
 export { getQuestionById } from "./model/mock-questions";
 export type { Question, QuestionFormValues, QuestionTag } from "./model/types";
