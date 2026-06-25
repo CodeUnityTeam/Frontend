700fd61c6d32b937db50a8511eea228a8f42dc42
Elena Ishmukhametova <renbiwlen@gmail.com>
2026-06-25 14:01:29 +0300

    Добавлена структура папок

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,10 +1,16 @@
 import { HomeBanner } from "@/widgets/home-banner";
 import { ValueProps } from "@/widgets/value-props";
 import { ChatbotCards } from "@/widgets/chatbot-content";
+// import { ProjectModal } from "@/features/project-modal";
 
 const HomePage = () => {
   return (
     <>
+    {/* <ProjectModal
+  open={true}
+  onOpenChange={(()=>{})}
+  mode="create"        
+/> */}
       <HomeBanner />
       <ChatbotCards />
       <ValueProps />
