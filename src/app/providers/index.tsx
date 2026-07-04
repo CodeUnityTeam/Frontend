7ddcec06fa29722ad81af60af59a86529c4a5318
Daniil Glushakov <anydnny@gmail.com>
2026-07-04 19:24:53 +0300

    Удален Tanstack Query Devtools

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "@/shared/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +12,6 @@ export function Providers({ children }: ProvidersProps) {
     <QueryClientProvider client={queryClient}>
       {children}
       <Toaster closeButton />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
