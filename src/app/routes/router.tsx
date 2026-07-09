f30d9df08c21b62cd3b879011797fc0f1136ce27
Gulnaz <gulnaz.davletshina.97@mail.ru>
2026-07-10 00:12:50 +0300

    фикс импортов

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -4,55 +4,194 @@ import { App } from "@/app/app";
 import { ProtectedRoute } from "@/shared/ui/protected-route/protected-route";
 import { ROUTES } from "@/shared/model/routes";
 
-
-const HomePage = lazy(() => import("@/pages/home/ui/home-page").then(m => ({ default: m.Component })));
-const RegisterPage = lazy(() => import("@/pages/register/ui/register-page").then(m => ({ default: m.Component })));
-const CheckEmailPage = lazy(() => import("@/pages/register/ui/check-email-page").then(m => ({ default: m.Component })));
-const VerifyEmailPage = lazy(() => import("@/pages/register/ui/verify-email-page").then(m => ({ default: m.Component })));
-const ResetPasswordConfirmPage = lazy(() => import("@/pages/reset-password/reset-password-confirm-page").then(m => ({ default: m.Component})));
-const ProjectsPage = lazy(() => import("@/pages/projects/projects-page").then(m => ({ default: m.Component })));
-const ProjectDetails = lazy(() => import("@/pages/project-details/project-details").then(m => ({ default: m.Component })));
-const QAPage = lazy(() => import("@/pages/qa/qa-page").then(m => ({ default: m.Component })));
-const QADetailsPage = lazy(() => import("@/pages/qa-details/qa-details-page").then(m => ({ default: m.Component })));
-const QACreatePage = lazy(() => import("@/pages/qa-create/qa-create-page").then(m => ({ default: m.Component })));
-const QAEditPage = lazy(() => import("@/pages/qa-edit/qa-edit-page").then(m => ({ default: m.Component })));
-const AboutPage = lazy(() => import("@/pages/about/about-page").then(m => ({ default: m.Component })));
-const HelpPage = lazy(() => import("@/pages/help/help-page").then(m => ({ default: m.Component })));
-const DocumentsPage = lazy(() => import("@/pages/documents/documents-page").then(m => ({ default: m.Component })));
-const ProfilePage = lazy(() => import("@/pages/profile/profile-page").then(m => ({ default: m.Component })));
-const SettingsPage = lazy(() => import("@/pages/settings/settings-page").then(m => ({ default: m.Component })));
-const AccountPage = lazy(() => import("@/pages/account/account-page").then(m => ({ default: m.Component })));
-const Page403 = lazy(() => import("@/pages/error-pages/page-403").then(m => ({ default: m.Component })));
-const Page404 = lazy(() => import("@/pages/error-pages/page-404").then(m => ({ default: m.Component })));
-const Page500 = lazy(() => import("@/pages/error-pages/page-500").then(m => ({ default: m.Component })));
+const RegisterPage = lazy(() =>
+  import("@/pages/register/ui/register-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const CheckEmailPage = lazy(() =>
+  import("@/pages/register/ui/check-email-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const VerifyEmailPage = lazy(() =>
+  import("@/pages/register/ui/verify-email-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const ResetPasswordConfirmPage = lazy(() =>
+  import("@/pages/reset-password/reset-password-confirm-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const ProjectsPage = lazy(() =>
+  import("@/pages/projects/projects-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const ProjectDetails = lazy(() =>
+  import("@/pages/project-details/project-details").then((m) => ({
+    default: m.Component,
+  })),
+);
+const QAPage = lazy(() =>
+  import("@/pages/qa/qa-page").then((m) => ({ default: m.Component })),
+);
+const QADetailsPage = lazy(() =>
+  import("@/pages/qa-details/qa-details-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const QACreatePage = lazy(() =>
+  import("@/pages/qa-create/qa-create-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const QAEditPage = lazy(() =>
+  import("@/pages/qa-edit/qa-edit-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const ProfilePage = lazy(() =>
+  import("@/pages/profile/profile-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const SettingsPage = lazy(() =>
+  import("@/pages/settings/settings-page").then((m) => ({
+    default: m.Component,
+  })),
+);
+const AccountPage = lazy(() =>
+  import("@/pages/account/account-page").then((m) => ({
+    default: m.Component,
+  })),
+);
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <HomePage /> },
-      { path: ROUTES.ONBOARDING, element: <ProtectedRoute><RegisterPage /></ProtectedRoute> },
+      {
+        index: true,
+        lazy: () => import("@/pages/home/ui/home-page"),
+      },
+      {
+        path: ROUTES.ONBOARDING,
+        element: (
+          <ProtectedRoute>
+            <RegisterPage />
+          </ProtectedRoute>
+        ),
+      },
       { path: ROUTES.REGISTER_CHECK_EMAIL, element: <CheckEmailPage /> },
       { path: ROUTES.REGISTER_VERIFY_EMAIL, element: <VerifyEmailPage /> },
-      { path: ROUTES.REGISTER_VERIFY_EMAIL_FALLBACK, element: <VerifyEmailPage /> },
-      { path: ROUTES.RESET_PASSWORD_CONFIRM, element: <ResetPasswordConfirmPage />},
-      { path: ROUTES.PROJECTS, element: <ProtectedRoute><ProjectsPage /></ProtectedRoute> },
-      { path: "projects/:id", element: <ProtectedRoute><ProjectDetails /></ProtectedRoute> },
-      { path: ROUTES.QA, element: <ProtectedRoute><QAPage /></ProtectedRoute> },
-      { path: ROUTES.QA_DETAILS, element: <ProtectedRoute><QADetailsPage /></ProtectedRoute> },
-      { path: ROUTES.QA_CREATE, element: <ProtectedRoute><QACreatePage /></ProtectedRoute> },
-      { path: ROUTES.QA_EDIT, element: <ProtectedRoute><QAEditPage /></ProtectedRoute> },
-      { path: ROUTES.ABOUT, element: <ProtectedRoute><AboutPage /></ProtectedRoute> },
-      { path: ROUTES.HELP, element: <ProtectedRoute><HelpPage /></ProtectedRoute> },
-      { path: ROUTES.DOCUMENTS, element: <ProtectedRoute><DocumentsPage /></ProtectedRoute> },
-      { path: ROUTES.PROFILE, element: <ProtectedRoute><ProfilePage /></ProtectedRoute> },
-      { path: ROUTES.SETTINGS, element: <ProtectedRoute><SettingsPage /></ProtectedRoute> },
-      { path: ROUTES.ACCOUNT, element: <ProtectedRoute><AccountPage /></ProtectedRoute> },
-      { path: ROUTES.FORBIDDEN, element: <Page403 /> },
-      { path: ROUTES.NOT_FOUND, element: <Page404 /> },
-      { path: ROUTES.SERVER_ERROR, element: <Page500 /> },
-      { path: "*", element: <Page404 /> },
+      {
+        path: ROUTES.REGISTER_VERIFY_EMAIL_FALLBACK,
+        element: <VerifyEmailPage />,
+      },
+      {
+        path: ROUTES.RESET_PASSWORD_CONFIRM,
+        element: <ResetPasswordConfirmPage />,
+      },
+      {
+        path: ROUTES.PROJECTS,
+        element: (
+          <ProtectedRoute>
+            <ProjectsPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "projects/:id",
+        element: (
+          <ProtectedRoute>
+            <ProjectDetails />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.QA,
+        element: (
+          <ProtectedRoute>
+            <QAPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.QA_DETAILS,
+        element: (
+          <ProtectedRoute>
+            <QADetailsPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.QA_CREATE,
+        element: (
+          <ProtectedRoute>
+            <QACreatePage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.QA_EDIT,
+        element: (
+          <ProtectedRoute>
+            <QAEditPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.ABOUT,
+        lazy: () => import("@/pages/about/about-page"),
+      },
+      {
+        path: ROUTES.HELP,
+        lazy: () => import("@/pages/help/help-page"),
+      },
+      {
+        path: ROUTES.DOCUMENTS,
+        lazy: () => import("@/pages/documents/documents-page"),
+      },
+      {
+        path: ROUTES.PROFILE,
+        element: (
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.SETTINGS,
+        element: (
+          <ProtectedRoute>
+            <SettingsPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.ACCOUNT,
+        element: (
+          <ProtectedRoute>
+            <AccountPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.FORBIDDEN,
+        lazy: () => import("@/pages/error-pages/page-403"),
+      },
+      {
+        path: ROUTES.NOT_FOUND,
+        lazy: () => import("@/pages/error-pages/page-404"),
+      },
+      {
+        path: ROUTES.SERVER_ERROR,
+        lazy: () => import("@/pages/error-pages/page-500"),
+      },
+      { path: "*", lazy: () => import("@/pages/error-pages/page-404") },
     ],
   },
 ]);
