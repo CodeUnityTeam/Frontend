4a66acfab42ab76cf83b4bc4311e252797b4cc1e
Mikhail Shilkin <mihschilkin@yandex.ru>
2026-07-31 18:17:42 +0400

    refactor(profile): единый useUpdateProfile, убран дубль в edit-profile-modal

--- src/features/edit-profile-modal/index.ts
+++ src/features/edit-profile-modal/index.ts
@@ -1,3 +1,2 @@
 export { EditProfileModal } from "./ui/edit-profile-modal";
-export { useProfile, useUpdateProfile, profileKeys } from "./model/use-edit-profile";
 export type { EditProfileFormData, UserProfileData } from "./model/types";
